0429b521e490e598bd43a19cab45b094dfd14c33
Nik <lavrish-nikita@yandex.ru>
2020-05-21 23:00:39 +0300

    Updates

---
+++ precache-manifest.1e1075ce3242db9284c578ecc6988412.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270ba508451cf836760926820300a0c7",
+    "url": "/testMerehead/index.html"
+  },
+  {
+    "revision": "3fd122f3e405c586f869",
+    "url": "/testMerehead/static/css/main.eeb8125c.chunk.css"
+  },
+  {
+    "revision": "1066eae8ddfbc83bba58",
+    "url": "/testMerehead/static/js/2.558d7c80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/testMerehead/static/js/2.558d7c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd122f3e405c586f869",
+    "url": "/testMerehead/static/js/main.6a00bdee.chunk.js"
+  },
+  {
+    "revision": "f02a1b754977384943ac",
+    "url": "/testMerehead/static/js/runtime-main.10f18495.js"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/testMerehead/static/media/man-1.bd80794f.png"
+  }
+]);
